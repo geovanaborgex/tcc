51c7e455d20307fe97d90983a041843b42f1948d
Geovana Borges <geovanaborges304@gmail.com>
2024-12-06 12:07:20 -0300

    atualizado dnv

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('tipo')->nullable(); // Tipo de usuário (opcional)
+            $table->foreignId('aluno_id')->nullable()->constrained('aluno')->onDelete('cascade');
+            $table->foreignId('profissional_id')->nullable()->constrained('profissional')->onDelete('cascade');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
